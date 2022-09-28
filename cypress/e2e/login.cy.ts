52f47e37d4de371f0dfaba6cc585ff7e933dd9f2
messiasGeovani <messias.geovani.lima@gmail.com>
2022-09-28 16:41:35 -0300

    login e2e tests

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -1,19 +1,38 @@
 /// <reference types="cypress" />
 
 describe("Login screen", () => {
-  const emailInput = "[data-test='AuthForm:email-input']";
-  const passwordInput = "[data-test='AuthForm:password-input']";
+  const elements = {
+    link: "[data-test='AuthForm:link']",
+    emailInput: "[data-test='AuthForm:email-input']",
+    passwordInput: "[data-test='AuthForm:password-input']",
+    submitButton: "button[type='submit']",
+  };
+
+  const data = {
+    email: "messias@email.com",
+    password: "1234",
+  };
 
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
   });
 
-  it("should fill login form fields and go to profile screen", () => {
-    const login = "messias@email.com";
-    const password = "1234";
+  it("should not fill all required fields and block the signup process", () => {
+    cy.url().should("include", "/login");
 
-    cy.get(emailInput).type(`${login}`);
-    cy.get(passwordInput).type(`${password}{enter}`);
+    cy.get(elements.submitButton).click();
+
+    cy.get("form :invalid").should("exist");
+
+    cy.get(elements.emailInput).type(`${data.email}{enter}`);
+    cy.get("form :invalid").should("exist");
+
+    cy.url().should("include", "/login");
+  });
+
+  it("should fill login form fields and go to profile screen", () => {
+    cy.get(elements.emailInput).type(`${data.email}`);
+    cy.get(elements.passwordInput).type(`${data.password}{enter}`);
     cy.url().should("include", "/profile");
   });
 });
--- cypress/e2e/signup.cy.ts
+++ cypress/e2e/signup.cy.ts
@@ -4,28 +4,52 @@ describe("Signup screen", () => {
   const elements = {
     link: "[data-test='AuthForm:link']",
     emailInput: "[data-test='AuthForm:email-input']",
-    passwordInput: "[data-test='AuthForm:confirm-password-input']",
+    passwordInput: "[data-test='AuthForm:password-input']",
     confirmPasswordInput: "[data-test='AuthForm:confirm-password-input']",
     accepTermsCheckbox: "[data-test='AuthForm:accept-terms-checkbox']",
     submitButton: "button[type='submit']",
+  };
+
+  const data = {
+    email: "messias@email.com",
+    password: "1234",
   };
 
   beforeEach(() => {
     cy.visit("http://localhost:3000/");
   });
 
-  it("should fill login form fields and go to profile screen", () => {
-    const login = "messias@email.com";
-    const password = "1234";
-
+  it("should not fill all required fields and block the signup process", () => {
     cy.get(elements.link).click();
     cy.url().should("include", "/signup");
 
     cy.wait(500);
 
-    cy.get(elements.emailInput).type(`${login}`);
-    cy.get(elements.passwordInput).type(`${password}`);
-    cy.get(elements.confirmPasswordInput).type(`${password}`);
+    cy.get(elements.submitButton).click();
+
+    cy.get("form :invalid").should("exist");
+
+    cy.get(elements.emailInput).type(`${data.email}{enter}`);
+    cy.get("form :invalid").should("exist");
+
+    cy.get(elements.passwordInput).type(`${data.password}{enter}`);
+    cy.get("form :invalid").should("exist");
+
+    cy.get(elements.confirmPasswordInput).type(`${data.password}{enter}`);
+    cy.get("form :invalid").should("exist");
+
+    cy.url().should("include", "/signup");
+  });
+
+  it("should fill login form fields and go to profile screen", () => {
+    cy.get(elements.link).click();
+    cy.url().should("include", "/signup");
+
+    cy.wait(500);
+
+    cy.get(elements.emailInput).type(`${data.email}`);
+    cy.get(elements.passwordInput).type(`${data.password}`);
+    cy.get(elements.confirmPasswordInput).type(`${data.password}`);
     cy.get(elements.accepTermsCheckbox).check();
     cy.get(elements.submitButton).click();
 
